3ff68a74956fd4683fdd0c102e2e48921fd7f73f
Diogo Reis <jodioguin.r@gmail.com>
2023-09-09 10:52:21 -0300

    Cração do arquivo de hello

---
+++ hello.py
@@ -0,0 +1,9 @@
+print('Hello World')
+
+nome = "Diogo"
+profissao = "Estudante e programador"
+
+print(f"Este é o primeiro programa de {nome}")
+print("eu sou" , profissao , "e decidi ir pelo caminho de python")
+
+resposta = input("Você está pronto para vir vomigo? (S/N):")
