4ff4d6b5cdcd5f2958284e653719f26e59d2386c
Diogo Reis <jodioguin.r@gmail.com>
2023-09-09 17:30:46 -0300

    verificação de idade

---
+++ verificacao_idade.py
@@ -0,0 +1,9 @@
+print("VERIFICAÇÃO DE IDADE")
+print("======================================")
+
+idade = int(input("Insira sua idade:"))
+
+if idade >= 18:
+    print("Você é maior de idade")
+else:
+    print("Você é menor de idade")
