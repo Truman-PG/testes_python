d10e31da621618c1e2ce76b0328627e7d2c90995
Diogo Reis <jodioguin.r@gmail.com>
2023-09-09 17:31:00 -0300

    mudança no imc

--- Programa.py
+++ Programa.py
@@ -4,7 +4,7 @@ nome = input("Qual o seu nome:")
 idade = int(input("Qual é a sua idade:"))
 peso = float(input("Qual o seu peso em Kg:"))
 altura = float(input("Qual a sua altura:"))
-imc = peso/(altura**2)
+imc = peso / (altura**2)
 
 print(f"Olá {nome}")
 print(f"Sua idade é de {idade}", "anos")
