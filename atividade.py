91b894fdca6cda0f6c3bb48040dc335980c591c2
Diogo Reis <jodioguin.r@gmail.com>
2023-09-09 17:31:42 -0300

    atividades

---
+++ atividade.py
@@ -0,0 +1,36 @@
+# Atividade 1
+
+a = 7
+b = 2
+c = 4 
+d = 14
+
+# 1.
+print(f"a + b - c, terá o resultado de: {a + b - c}")
+
+# 2.
+print(f"A * B / C terá o resultaddo de: {a * b / c}")
+
+#3
+print(f"C + 22 * D terá o resuldado de: {c+22*d}")
+
+# 4.
+print(f"A + B * C + D terá o resultado de: {a + b * c + d}")
+
+# 5. 
+print(f"(A + B) * (C + D) terá o resultado de: {(a + b) * (c + d)}")
+
+# 6.
+print(f"(D / A) / (B / C) terá o resultado de: {(d / a) / (b / c)}")
+
+# 7.
+print(f"10 ** A terá o resultado de: {10**a}")
+
+# 8.
+print(f"58 % A terá o resultado de: {58%a}")
+
+# 9.
+print(f"(A + B + C * D) / (B - C * (B - C)) terá o resultado de: {(a+b+c*d)/(b-c*(b-c))}")
+
+# 10.
+print(f"(A + B + C * D) / (B - C * (B - C)) + 10 ** A terá o resultado de: {(a+b+c*d)/(b-c*(b-c))+10**a}")
