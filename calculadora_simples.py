d0acc7cc0ed5f84e86732d858d21e53d9b723b11
Diogo Reis <jodioguin.r@gmail.com>
2023-09-09 17:32:04 -0300

    completando a calculadora

---
+++ calculadora_simples.py
@@ -0,0 +1,22 @@
+numero1 = float(input("Insira um número:"))
+numero2 = float(input("Insira um número:"))
+operacao = input("Insira a operação para calcular os números (+ - * /):")
+
+if operacao == "+" :
+resultado = numero1+numero2
+print(f"O resultado é de:{resultado}")
+
+elif operacao == "-" 
+resultado2 = numero1-numero2
+print(f"O resultado é de:{resultado2}")
+
+elif operacao == "*" 
+resultado3 = numero1*numero2
+print(f"O resultado é de:{resultado3}")
+
+elif operação == "/" 
+resultado4 = numero1/numero2
+print("O resultado é de:{resultado4}")
+
+else:
+print("Não é possivel ser executado")
