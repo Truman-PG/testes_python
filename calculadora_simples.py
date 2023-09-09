4e3f14d2e834c3be7d3b4a13663e62bc7add68d2
Diogo Reis <jodioguin.r@gmail.com>
2023-09-09 17:37:31 -0300

    Concluindo a calculadora simples

--- calculadora_simples.py
+++ calculadora_simples.py
@@ -3,20 +3,20 @@ numero2 = float(input("Insira um número:"))
 operacao = input("Insira a operação para calcular os números (+ - * /):")
 
 if operacao == "+" :
-resultado = numero1+numero2
-print(f"O resultado é de:{resultado}")
+    resultado = numero1+numero2
+    print(f"O resultado é de:{resultado}")
 
-elif operacao == "-" 
-resultado2 = numero1-numero2
-print(f"O resultado é de:{resultado2}")
+elif operacao == "-" :
+    resultado = numero1-numero2
+    print(f"O resultado é de:{resultado}")
 
-elif operacao == "*" 
-resultado3 = numero1*numero2
-print(f"O resultado é de:{resultado3}")
+elif operacao == "*" :
+    resultado = numero1*numero2
+    print(f"O resultado é de:{resultado}")
 
-elif operação == "/" 
-resultado4 = numero1/numero2
-print("O resultado é de:{resultado4}")
+elif operacao == "/" :
+    resultado = numero1/numero2
+    print(f"O resultado é de:{resultado}")
 
 else:
-print("Não é possivel ser executado")
+    print("Não é possivel ser executado")
