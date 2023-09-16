f6d491fb64207bc001c23890cf7c602a5546cfe2
Diogo Reis <jodioguin.r@gmail.com>
2023-09-16 10:14:07 -0300

    Alteração no codigo

--- calculadora_simples.py
+++ calculadora_simples.py
@@ -1,22 +1,20 @@
 numero1 = float(input("Insira um número:"))
 numero2 = float(input("Insira um número:"))
 operacao = input("Insira a operação para calcular os números (+ - * /):")
+resultado = ''
 
-if operacao == "+" :
-    resultado = numero1+numero2
-    print(f"O resultado é de:{resultado}")
-
-elif operacao == "-" :
-    resultado = numero1-numero2
-    print(f"O resultado é de:{resultado}")
-
-elif operacao == "*" :
-    resultado = numero1*numero2
-    print(f"O resultado é de:{resultado}")
-
-elif operacao == "/" :
-    resultado = numero1/numero2
-    print(f"O resultado é de:{resultado}")
-
+if operacao == '+':
+    resultado = numero1 + numero2
+elif operacao == '-':
+    resultado = numero1 - numero2
+elif operacao == '*':
+    resultado = numero1 * numero2
+elif operacao == '/':
+    if numero2 == 0:
+        print("Não é possível dividir por zero")
+    else:
+        resultado = numero1 / numero2
 else:
-    print("Não é possivel ser executado")
+    print("Operação não reconhecida.")
+if resultado != '':
+    print(f"O resultado é{resultado}")
